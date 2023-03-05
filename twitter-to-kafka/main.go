1d59f904629edb11460cb943c43461fd69e1c251
sanghyuk lee <sanghyuk.chris.lee@gmail.com>
2023-03-05 16:33:07 -0500

    Use http while waiting for Twitter's approval

--- twitter-to-kafka/main.go
+++ twitter-to-kafka/main.go
@@ -1,29 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"twitter-to-kafka/packages/config"
+	"io/ioutil"
+	"log"
+	"net/http"
+	// twitterstream "github.com/fallenstedt/twitter-stream"
 )
 
 func main() {
 
-	// vp := viper.New()
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("HELLO")
 
-	// vp.SetConfigFile("./test.json")
-	// err := vp.ReadInConfig()
+		d, _ := ioutil.ReadAll(r.Body)
+
+		log.Printf("Data %s\n", d)
+
+	})
+
+	http.ListenAndServe(":9090", nil)
+
+	// config, err := config.LoadConfig()
 	// if err != nil {
 	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(config.Twitter.API_KEY)
+	// 	fmt.Println(config.Twitter.SECRET)
 	// }
-
-	// fmt.Println(vp.GetString("foo"))
-
-	config, err := config.LoadConfig()
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(config.Twitter.API_KEY)
-		fmt.Println(config.Twitter.SECRET)
-	}
 
 	// tok, err := twitterstream.NewTokenGenerator().SetApiKeyAndSecret("key", "secret").RequestBearerToken()
 
